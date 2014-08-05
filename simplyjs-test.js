27e5b5a049f096ec014682b8f30c80e08dbb09b9
eharrow <ewan@Okka.local>
2014-08-05 21:03:46 +0100

    more simplyjs copied

--- simplyjs-test.js
+++ simplyjs-test.js
@@ -1,3 +1,11 @@
 simply.title('Hello World!');
 
 simply.vibe('short');
+
+simply.on('singleClick', function(e) {
+  simply.subtitle('You pressed the ' + e.button + ' button!');
+});
+
+simply.on('longClick', function(e) {
+  simply.subtitle('You held the ' + e.button + ' button!');
+});
